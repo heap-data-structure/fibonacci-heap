f01a5737b89782d249c5c1ecb3a01390112239bc
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-15 14:10:28 +0200

    :boom: refactor!: Use flat export style.
    BREAKING CHANGE: We dropped the default export. It would not work anyway
    due to microbundle.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import Node from './Node.js';
-import FibonacciHeap from './FibonacciHeap.js';
-
-export default FibonacciHeap;
-
-export {Node, FibonacciHeap};
+export {default as FibonacciHeap} from './FibonacciHeap.js';
+export {default as Node} from './Node.js';
+export {default as cascading_cut} from './cascading_cut.js';
+export {default as consolidate} from './consolidate.js';
+export {default as cut} from './cut.js';
+export {default as link} from './link.js';
+export {default as list_reset_parent} from './list_reset_parent.js';
